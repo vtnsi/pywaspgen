562223e56ccc3866771b93b5d3f5839a3b6ac460
Alyse Jones <alysemjones@1311-2057-JONES.ad.hume.vt.edu>
2025-04-18 13:47:18 -0400

    adding print statement to debug fading

--- pywaspgen/iq_datagen.py
+++ pywaspgen/iq_datagen.py
@@ -100,6 +100,8 @@ class IQDatagen:
         new_burst_list = []
         for burst in burst_list:
             samples, sig_modem = self._get_iq(burst)
+            print(10*np.log10(np.mean(np.abs(samples)**2)))
+
             burst.duration = len(samples)
             start_iq_idx = max(0, burst.start)
             start_samples_idx = max(0, -burst.start)
