8ba4d6cdb7eae62f713a115dc4d7e77d6c1c1945
Jacob Dennis <heeroyuy@vt.edu>
2025-06-25 13:10:02 -0400

    Initial commit to have yolo ready dataset

--- examples/data_generator.py
+++ examples/data_generator.py
@@ -16,6 +16,7 @@ import signal
 from pathlib import Path
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 import pywaspgen.impairments as impairments
 from pywaspgen.burst_datagen import BurstDatagen
@@ -23,6 +24,17 @@ from pywaspgen.iq_datagen import IQDatagen
 
 # Just for sync / gracefull spindown
 is_alive = True
+
+label_dict={"BASK":0,
+        "16ASK":1,
+        "4PAM":2,
+        "16PAM":3,
+        "QPSK":4,
+        "8PSK":5,
+        "16PSK":6,
+        "16QAM":7,
+        "ALT-QPSK":8,
+        }
 
 
 def signal_handler(sig, frame):
@@ -53,6 +65,16 @@ def parse_args():
     parser.add_argument("--batch-size", dest="batch_size", type=int, default=10, help="Batch the IQ generation into groups of <Batch_Size>")
     return parser.parse_args()
 
+def graph_specgram(iq, fname):
+    fig,ax = plt.subplots(1)
+    fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
+    fig.set_size_inches(1000.0 / plt.rcParams["figure.dpi"], 500.0 / plt.rcParams["figure.dpi"])
+    ax.axis('off')
+    pxx, freqs, bins, im = ax.specgram(x=iq, Fs=1.0, noverlap=64, NFFT=512)
+    ax.axis('off')
+    fig.savefig(fname, dpi=640, transparent=True)
+    plt.close()
+
 
 def _gen_save_off(burst_entry, iq_data):
     """
@@ -60,6 +82,7 @@ def _gen_save_off(burst_entry, iq_data):
     We've assumed perfect isolation and shifting down to baseband.
     """
 
+    yolo_label_listing = []
     for b in burst_entry:
         corrected_iq_data = iq_data[b.get_start_time() : b.get_end_time()]  # Corrects the sample offset of the received samples.
         corrected_iq_data = impairments.freq_off(corrected_iq_data, -b.get_center_freq())  # Corrects the frequency offset of the received samples.
@@ -70,6 +93,27 @@ def _gen_save_off(burst_entry, iq_data):
         np.save(args.folder_path_out + "/iq/" + unique_id, corrected_iq_data)
         with open(args.folder_path_out + "/meta/" + unique_id + ".pkl", "wb") as f:
             pickle.dump(b, f)
+
+        x_cent = b.cent_freq+0.5
+        y_cent = b.get_mid_time()/len(iq_data)
+        width = b.bandwidth
+        height = b.duration/len(iq_data)
+        label = label_dict[b.get_meta_label()]
+        annotation = f"{label} {x_cent:.6f} {y_cent:.6f} {width:.6f} {height:.6f}"
+        yolo_label_listing.append(annotation)
+    
+    # Now Save off the Image
+    # Now Save off the Label
+    #plt.specgram(iq_data, 256, 1.0, noverlap=64, cmap="plasma")
+    graph_specgram(iq=iq_data,fname=args.folder_path_out + "/images/" + unique_id + ".png")
+    #plt.savefig(args.folder_path_out + "/images/" + unique_id + ".png")
+    #plt.close()
+    
+    with open(args.folder_path_out + "/labels/" + unique_id + ".txt","w") as f:
+        for annot in yolo_label_listing:
+            f.write(annot + "\n")
+        
+ 
 
 
 def main(args):
@@ -104,8 +148,12 @@ if __name__ == "__main__":
     args = parse_args()
     file_path_meta = args.folder_path_out + "/meta/"
     file_path_iq = args.folder_path_out + "/iq/"
+    file_path_img = args.folder_path_out + "/images/"
+    file_path_labels = args.folder_path_out + "/labels/"
 
     Path(file_path_meta).mkdir(parents=True, exist_ok=True)
     Path(file_path_iq).mkdir(parents=True, exist_ok=True)
+    Path(file_path_img).mkdir(parents=True, exist_ok=True)
+    Path(file_path_labels).mkdir(parents=True, exist_ok=True)
 
     main(args)
